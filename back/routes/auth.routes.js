22bcb99b1255ae41aeb68cb1ced63fe75ff5855e
chuinga <mfproencamartins@gmail.com>
2024-01-22 16:46:00 +0000

    Set up routes

--- back/routes/auth.routes.js
+++ back/routes/auth.routes.js
@@ -1,36 +1,59 @@
-const router = require('express').Router()
-const jwt = require('jsonwebtoken')
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-router.post('/signup', (req, res, next) => {
+const SALT_ROUNDS = 15;
+
+router.post('/signup', async (req, res, next) => {
   /* Get back the payload from your request, as it's a POST you can access req.body */
-
+  const payload = req.body;
   /* Hash the password using bcryptjs */
-
+  const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+  const passwordHash = bcrypt.hashSync(payload.password, salt);
   /* Record your user to the DB */
-
-  res.json('Pinging signup')
+  const userToRegister = { email: payload.email, passwordHash };
+  try {
+    const newUser = await UserActivation.create(userToRegister);
+    res.status(201).json({ message: 'User created', newUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 })
 
-router.post('/login', (req, res, next) => {
+router.post('/login', async (req, res, next) => {
   /* Get back the payload from your request, as it's a POST you can access req.body */
-
+  const payload = req.body;
   /* Try to get your user from the DB */
-
+  try {
+    const potentialUser = await UserActivation.findOne({ email: payload.email.toLowerCase().trim() });
   /* If your user exists, check if the password is correct */
-
-  /* If your password is correct, sign the JWT using jsonwebtoken */
-  const authToken = jwt.sign(
-    {
-      expiresIn: '6h',
-      user: null, // Put yhe data of your user in there
-    },
-    process.env.TOKEN_SECRET,
-    {
-      algorithm: 'HS256',
+    if (potentialUser) {
+      if (bcrypt.compareSync(payload.password, potentialUser.passwordHash)) {
+  /* If your password is correct, sign the JWT using jsonwebtoken */   
+        const authToken = jwt.sign(
+          {
+            userId: potentialUser._id,            
+          }, 
+          process.env.TOKEN_SECRET,
+          {
+            algorithm: 'HS256',
+            expiresIn: '8h',
+          }
+        )
+        res.status(200).json({ token: authToken });
+      }
+      }
+      else {
+        res.status(403).json({ message: 'Wrong Password' });
+      }
+    else {
+      res.status(404).json({ message: ' Wrong User' });
     }
-  )
-
-  res.json('Pinging login')
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }  
 })
 
 router.get('/verify', (req, res, next) => {
